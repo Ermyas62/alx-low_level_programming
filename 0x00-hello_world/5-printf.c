4c390047d44d29e16228c18701b1e4e798cf46dc
Ermyas62 <ermyas62@gmail.com>
2023-09-07 03:52:39 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main -print with proper grammar using printf followed by new line
+ * Return: 0(integer)
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n")
+return 0;
+}
