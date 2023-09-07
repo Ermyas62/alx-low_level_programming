aff98e01236a1c2e673b908555237bb5c0059783
Ermyas62 <ermyas62@gmail.com>
2023-09-07 03:41:41 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
- * main: print "Programming is like building a multilingual puzzle.
- * Return: 0(return integer)
+ * main - print "Programming is like building a multilingual puzzle.
+ * Return: 0(returns integer)
  */
 int main(void)
 {
