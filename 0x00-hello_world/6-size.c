aa7761455f2958e418b299add05b7fa6e034cca1
Ermyas62 <ermyas62@gmail.com>
2023-09-07 04:19:11 -0700

    size

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**main - print the size of the computer
+ * Return: always 0
+ */
+
+int main(void)
+{
+printf("size of a char: %zu byte(s)\n", sizeof(char));
+printf("size of an int: %zu byte(s)\n, sizeof(int));
+printf("size of a long int: %zu byte(s)\n", sizeof(long int));
+printf("size of a float: %zu byte(s)\n", sizeof(float));
+return (0);
