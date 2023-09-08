b45ebfc35c2cfea0185d6e417d31231664c5a043
Ermyas62 <ermyas62@gmail.com>
2023-09-08 10:56:07 -0700

    alphabet game

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ *  main - print the alphabatic
+ *  Return: Always0
+ */
+
+int main(void)
+
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	putchar(c);
+	putchar('\n');
+	return (0);
+
+}
