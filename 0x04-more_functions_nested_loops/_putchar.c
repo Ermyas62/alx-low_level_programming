dcb9ccbdd3c8df7cb17ab0213aa7a143bc710d0d
Ermyas62 <ermyas62@gmail.com>
2023-09-14 23:38:54 -0700

    isupper

---
+++ 0x04-more_functions_nested_loops/_putchar.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * _putchar - write the character c to be stdout
+ * @c: the char to be print
+ * Return: 1 on success
+ * on error -1 is returned
+ */
+
+int _putchat(char c)
+{
+	return (write(1, &c, 1));
+}
