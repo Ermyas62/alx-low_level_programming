c4d7980554f4a1e8f0ebfae2b4021b499dbaa90c
Ermyas62 <ermyas62@gmail.com>
2023-09-16 02:07:07 -0700

    Buzz Fizz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -10,17 +10,17 @@ int main(void)
 {
 	int i;
 
-	for (i = 0; i <= 100; i++)
+	for (i = 1; i <= 100; i++)
 	{
 		if (i % 3 == 0 && i % 5 != 0)
 		{
-			printf("Fizz");
+			printf(" Fizz");
 		} else if (i % 5 == 0 && i % 3 != 0)
 		{
-			printf("Buzz");
+			printf(" Buzz");
 		} else if (i % 5 == 0 && i % 3 == 0)
 		{
-			printf("FizzBuzz");
+			printf(" FizzBuzz");
 		} else if (i == 1)
 		{
 			printf("%d", i);
@@ -29,5 +29,7 @@ int main(void)
 			printf("%d", i);
 		}
 	}
+	printf("\n");
+
 	return (0);
 }
