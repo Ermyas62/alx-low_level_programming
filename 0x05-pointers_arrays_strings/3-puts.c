c1c1596c65cf6af1aa39a563780279c3c0980b1b
Ermyas62 <ermyas62@gmail.com>
2023-09-18 04:58:06 -0700

    print a string

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string
+ * @str: the string to be print
+ * Return: void
+ */
+
+void _puts(char *str)
+
+{
+	for (; *str != '\n'; str++)
+	_putchar(*str);
+	_putchar('\n');
+}
