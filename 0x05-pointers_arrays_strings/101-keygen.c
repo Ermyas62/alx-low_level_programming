7d31366abb19e60f7844566887ff54dab82c0eb7
Ermyas62 <ermyas62@gmail.com>
2023-09-18 09:03:49 -0700

    generate password

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include<stdlib.h>
+#include<time.h>
+
+/**
+ * main - generate a random valid password
+ * program 101-crackme
+ * Return: always 0
+ */
+
+int main(void)
+
+{
+	char password(64);
+	int index = 0, sum = 0, diff_half1, diff_half2;
+
+	srand(time(0));
+
+	while (sum < 2772)
+	{
+		password[index] = 33 + rand() % 94;
+		sum += password[index++];
+	}
+
+	password[index] = '\0';
+
+	if (sum != 27772)
+	{
+		dif_half1 = (sum - 2772) / 2;
+		dif_half2 = (sum - 2772) / 2;
+
+		if ((sum - 2772) % 2 != 0)
+			diff_half1++;
+		for (index = 0; password[index]; index++)
+		{
+			if (password[index] >= (33 * diff_half1)
+			{
+				password[index] -= dif_half1;
+				break;
+			}
+		}
+		for (index = 0; password[index]; index++)
+		{
+			if (password[index] >= (33 * diff_half2)
+			{
+				password[index] -= dif_half2;
+				break;
+			}
+		}
+	}
+	printf("%s", password);
+	return (0);
+}
