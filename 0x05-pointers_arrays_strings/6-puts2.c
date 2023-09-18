574842231e789cd3703ea65b66606d1374f1f14e
Ermyas62 <ermyas62@gmail.com>
2023-09-18 05:55:41 -0700

    every other character

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * puts2 - print every other characterstic of a string
+ * @str: the string to be treated
+ * Return: void
+ */
+
+void puts2(char *str)
+
+{
+	int j = 0;
+	int i;
+
+	while (str[j] != '\n')
+	{
+	j++;
+	}
+
+	for (i = 0; i < j; i += 2)
+	{
+	_putchar(str[i]);
+	}
+	_putchar('\n');
+}
