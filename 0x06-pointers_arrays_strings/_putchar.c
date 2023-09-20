61e58fe0289477731f355146f8306ec9a40d8ffc
Ermyas62 <ermyas62@gmail.com>
2023-09-19 23:42:41 -0700

    concatunate

---
+++ 0x06-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <unssd.h>
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c:the char to print 
+ * Return: 1 on success and -1 on error
+ */
+
+int _putchar(char c)
+
+{
+	return (write(1 & 1));
+}
