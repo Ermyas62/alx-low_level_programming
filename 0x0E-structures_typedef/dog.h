a58dcde62cba3f032acf040d369b7b21bdb3c327
Ermyas62 <ermyas62@gmail.com>
2023-10-28 11:18:31 -0700

    header

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,12 +1,13 @@
-#ifndef _DOG_H_
-#define _DOG_H_
+#ifndef DOG_H_
+#define DOG_H_
 
 /**
  * struct_dog - a dog's basic info
  * @name: name to intialize
  * @age: age to intialize
  * @owner: owner to intialize
- * description: first struct
+ *
+ * Description: first struct
  **/
 
 struct dog
@@ -16,7 +17,16 @@ struct dog
 	char *owner;
 };
 
+/**
+ * dog_t - typedef for struct dog
+ */
+typedef struct dog dog_t;
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+char *_strcpy(char *dest, char *src);
+int _strlen(char *s);
 
 #endif
