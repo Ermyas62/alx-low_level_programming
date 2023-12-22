1ddeeeb4ec91d4217f8b0916a85213411a1b0eb1
Ermyas62 <ermyas62@gmail.com>
2023-12-21 23:40:39 -0800

    gives you the index of a key

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -10,10 +10,7 @@
 
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
-	unsigned long int hash_results;
+	unsigned long int hash = hash_djb2(key);
 
-	hash_results = hash_djb2(key);
-	if (size != 0)
-		return (hash_results % size);
-	return (0);
+	return (hash % size);
 }
